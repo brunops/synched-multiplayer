4186c6e3e07cea534249d4d6af3edd1c1de7fdf4
Bruno Sanches <brunopsanches@gmail.com>
2014-04-29 17:56:55 -0700

    Define MessageQueue#enqueue and MessageQueue#dequeue

--- MessageQueue.js
+++ MessageQueue.js
@@ -6,6 +6,16 @@ MessageQueue.prototype.init = function (opts) {
   opts = opts || {};
 
   this.latency = opts.latency || 0;
+  this.messages = [];
+};
+
+MessageQueue.prototype.enqueue = function (msg) {
+  msg.t = msg.t || Date.now();
+  this.messages.push(msg)
+};
+
+MessageQueue.prototype.dequeue = function () {
+  return this.messages.pop();
 };
 
 module.exports = MessageQueue;
--- test/MessageQueueTest.js
+++ test/MessageQueueTest.js
@@ -3,9 +3,16 @@ var test = require('tape');
 var MessageQueue = require('../MessageQueue');
 
 test(function (t) {
-  t.plan(1);
+  t.plan(2);
 
   var mq = new MessageQueue();
   t.equal(mq.latency, 0);
 
+  var msg = {
+    t: (new Date()).getTime(),
+    payload: 'sup, bro'
+  };
+  mq.enqueue(msg);
+
+  t.equal(mq.dequeue(), msg);
 });
