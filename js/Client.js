060d4a9c05a13e407586cd649156b47b5ad86727
Bruno Sanches <brunopsanches@gmail.com>
2014-05-06 18:13:33 -0700

    Handle keyboard input
    * Store keyboardState inside client instance as an object
    * bind key up and down events to set keyboardState
    * Define #cloneKeyboardState method in order to avoid messing with state when storing pendingInputs (close future :)
    * BOOM!

--- js/Client.js
+++ js/Client.js
@@ -15,6 +15,61 @@ Client.prototype.init = function (opts) {
   this.width = 500;
   this.height = 500;
   // ------------------------[end of temp code]
+
+  this.keyboardState = {};
+  this.bindEvents();
+};
+
+Client.prototype.bindEvents = function () {
+  this.bindKeyboardHandling();
+};
+
+Client.prototype.bindKeyboardHandling = function () {
+  var self = this;
+
+  document.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+      case 37:
+        self.keyboardState.LEFT = true;
+        break;
+      case 38:
+        self.keyboardState.UP = true;
+        break;
+      case 39:
+        self.keyboardState.RIGHT = true;
+        break;
+      case 40:
+        self.keyboardState.DOWN = true;
+        break;
+    }
+  });
+
+  document.addEventListener('keyup', function (e) {
+    switch (e.keyCode) {
+      case 37:
+        delete self.keyboardState.LEFT;
+        break;
+      case 38:
+        delete self.keyboardState.UP;
+        break;
+      case 39:
+        delete self.keyboardState.RIGHT;
+        break;
+      case 40:
+        delete self.keyboardState.DOWN;
+        break;
+    }
+  });
+};
+
+Client.prototype.cloneKeyboardState = function () {
+  var keyboardState = {};
+
+  for (var key in this.keyboardState) {
+    keyboardState[key] = true;
+  }
+
+  return keyboardState;
 };
 
 
