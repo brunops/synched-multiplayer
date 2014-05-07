504c95bc1633f4591db90a469278ce8860d910c1
Bruno Sanches <brunopsanches@gmail.com>
2014-05-06 18:28:58 -0700

    remove console.log. derp.

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ var Client = require('./Client');
 (function () {
   var canvasContext = document.getElementById('canvas').getContext('2d');
 
-  console.log(canvasContext)
-
   var client = new Client({
     context: canvasContext
   });
